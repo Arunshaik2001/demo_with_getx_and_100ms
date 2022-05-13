7520a52d0e23a895890d347b529b4522642b3cc7
MosesATJTC <shaik@100ms.live>
2022-05-13 13:37:56 +0530

    UI changes and remove peer issues fix

--- lib/views/PreviewWidget.dart
+++ lib/views/PreviewWidget.dart
@@ -27,12 +27,12 @@ class PreviewWidget extends StatelessWidget {
           GetX<PreviewController>(builder: (controller) {
             return Container(
               child: controller.localTracks.length > 0
-                  ? Container(
+                  ? SizedBox(
                       height: itemHeight,
                       width: itemWidth,
                       child: Stack(
                         children: [
-                          HMSVideoView(track: controller.localTracks[0]),
+                          HMSVideoView(track: controller.localTracks[0],matchParent: true,),
                           Positioned(
                             bottom: 20.0,
                             left: itemWidth / 2 - 50.0,
--- lib/views/RoomWidget.dart
+++ lib/views/RoomWidget.dart
@@ -4,74 +4,65 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RoomWidget extends StatelessWidget {
-  String meetingUrl;
-  String userName;
+  final String meetingUrl;
+  final String userName;
 
-  late RoomController roomController;
+  late final RoomController roomController;
 
   RoomWidget(this.meetingUrl, this.userName, {Key? key}) : super(key: key) {
     roomController = Get.put(RoomController(meetingUrl, userName));
   }
 
-  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GetX<RoomController>(builder: (controller) {
-        return Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: controller.usersList.length,
-                  itemBuilder: (ctx, index) {
-                    return Card(
-                        child: SizedBox(
-                            height: 300.0,
-                            child:
-                                VideoWidget(index)));
-                  }),
-            ),
-          ],
-        );
+        return ListView.builder(
+            itemCount: controller.usersList.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                  child:
+                  SizedBox(height: 250.0, child: VideoWidget(index)));
+            });
       }),
-      bottomNavigationBar: GetX<RoomController>(
-        builder: (controller) {
-          return BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: Colors.black,
-              selectedItemColor: Colors.greenAccent,
-              unselectedItemColor: Colors.grey,
-              items: <BottomNavigationBarItem>[
-                const BottomNavigationBarItem(
-                  icon: Icon(Icons.cancel),
-                  label: 'Leave Meeting',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(controller.isLocalAudioOn.value?Icons.mic:Icons.mic_off),
-                  label: 'Mic',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(controller.isLocalVideoOn.value?Icons.videocam:Icons.videocam_off),
-                  label: 'Camera',
-                ),
-              ],
-              currentIndex: _selectedIndex,
-              //New
-              onTap: _onItemTapped);
-        }
-      ),
+      bottomNavigationBar: GetX<RoomController>(builder: (controller) {
+        return BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.black,
+            selectedItemColor: Colors.greenAccent,
+            unselectedItemColor: Colors.grey,
+            items: <BottomNavigationBarItem>[
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.cancel),
+                label: 'Leave Meeting',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(controller.isLocalAudioOn.value
+                    ? Icons.mic
+                    : Icons.mic_off),
+                label: 'Mic',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(controller.isLocalVideoOn.value
+                    ? Icons.videocam
+                    : Icons.videocam_off),
+                label: 'Camera',
+              ),
+            ],
+
+            //New
+            onTap: _onItemTapped);
+      }),
     );
   }
 
   void _onItemTapped(int index) {
     if (index == 0) {
       roomController.leaveMeeting();
-    }
-    else if(index == 1){
+    } else if (index == 1) {
       roomController.toggleAudio();
-    }
-    else{
+    } else {
       roomController.toggleVideo();
     }
   }
